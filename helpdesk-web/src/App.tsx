51d25f03cb37b6a360a4a661a3c023cef265a6ae
FathanEmHa <fathanmh26@gmail.com>
2026-08-12 22:43:28 +0700

    feat: create tickets page

--- helpdesk-web/src/App.tsx
+++ helpdesk-web/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import MainLayout from "./components/layout/MainLayout";
 import LoginPage from "./features/auth/pages/LoginPage";
 import DashboardPage from "./features/dashboard/pages/DashboardPage";
+import TicketsPage from "./features/tickets/pages/TicketsPage";
 
 function App() {
   return (
@@ -18,6 +19,11 @@ function App() {
           <Route
             path="/dashboard"
             element={<DashboardPage />}
+          />
+
+          <Route
+            path="/tickets"
+            element={<TicketsPage />}
           />
         </Route>
       </Routes>
