b93ab2781747070f7d2465c5c305303796b446d9
FathanEmHa <fathanmh26@gmail.com>
2026-08-18 16:07:11 +0700

    feat: api fetch automate claim jwt from auth storage and include it to authorization header

--- helpdesk-web/src/lib/api.ts
+++ helpdesk-web/src/lib/api.ts
@@ -1,3 +1,5 @@
+import { getStoredAuth } from "../features/auth/authStorage";
+
 const API_URL = import.meta.env.VITE_API_URL;
 
 let unauthorizedHandler: (() => void) | null = null;
@@ -12,10 +14,17 @@ export async function apiFetch<T>(
   endpoint: string,
   options?: RequestInit,
 ): Promise<T> {
+  const auth = getStoredAuth();
+
   const response = await fetch(`${API_URL}${endpoint}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
+
+      ...(auth?.token && {
+        Authorization: `Bearer ${auth.token}`,
+      }),
+
       ...options?.headers,
     },
   });
